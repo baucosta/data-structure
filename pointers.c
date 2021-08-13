7fd05cc0ac88135f4ce590250892c083d7142d36
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2021-08-12 21:31:48 -0300

    Working with pointers

--- pointers.c
+++ pointers.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
 main() {
-    
+    int val1, val2, *val3, *val4;
+
+    val1 = 10;
+    val2 = 20;
+    val3 = &val1;
+    val4 = &val2;
+
+    printf("%d %d %d %d\n\n", val1, val2, val3, val4);
+
 }
