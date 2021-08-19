4e2257138909423b6d9fd9cfa7c8dc6592608e14
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2021-08-19 20:13:30 -0300

    Working with pointers

--- pointers.c
+++ pointers.c
@@ -1,13 +1,45 @@
 #include<stdio.h>
 
 main() {
-    int val1, val2, *val3, *val4;
+    int val1, val2;
+    int *val3, *val4;
 
     val1 = 10;
     val2 = 20;
     val3 = &val1;
     val4 = &val2;
 
-    printf("%d %d %d %d\n\n", val1, val2, val3, val4);
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
+
+    *val3 = 40;
+    *val4 = 50;
+
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
+
+    val1 = *val3 - 10;
+    val2 = *val4 - 20;
+
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
+
+    val2 = *val3 - 10;
+    val1 = (*val3 - 10) * 5;
+
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
+
+    val1 = *val3 - 10;
+    *val3 = 5;
+
+    //val1 = 90
+    // val3 = 5
+
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
+
+    val4 = val3;
+
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
+
+    val1 = 80;
+
+    printf("%d %d %d %d\n\n", val1, val2, *val3, *val4);
 
 }
